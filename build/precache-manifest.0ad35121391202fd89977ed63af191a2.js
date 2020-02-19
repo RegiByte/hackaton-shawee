781af624720cbe8ac3966308ec3ff81250478c08
Reginaldo Junior <reginaldo.junior696@gmail.com>
2020-02-18 21:31:27 -0300

    added build files

---
+++ build/precache-manifest.0ad35121391202fd89977ed63af191a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bb53a16a44971995165c737e2bf47f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2239f31e06d0740e741f",
+    "url": "/static/css/2.c5b05437.chunk.css"
+  },
+  {
+    "revision": "0a190d7a9c4104065a1b",
+    "url": "/static/css/main.de4a92e9.chunk.css"
+  },
+  {
+    "revision": "2239f31e06d0740e741f",
+    "url": "/static/js/2.b22d4a3f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b22d4a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a190d7a9c4104065a1b",
+    "url": "/static/js/main.8ea3aac3.chunk.js"
+  },
+  {
+    "revision": "927eb03666f3ca308397",
+    "url": "/static/js/runtime-main.8de4e9aa.js"
+  },
+  {
+    "revision": "9b8665bb37bc93384a19f6bfe304174b",
+    "url": "/static/media/profile.9b8665bb.png"
+  }
+]);
